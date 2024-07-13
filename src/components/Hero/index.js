e135502614545fe09b7b839832ed20d08a73240c
Christina Pagan <76018424+cpagan415@users.noreply.github.com>
2024-07-13 18:04:09 -0400

    updated linked in url deleted comments in html

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,22 +4,22 @@ import { HeroContainer, HeroBg, VideoEff, HeroContent, Heroh1, Heroul } from './
 import { motion } from 'framer-motion';
 
 
-function Hero(){
+function Hero() {
 
     return (
-        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.15}}exit={{opacity: 0}}>
-        <HeroContainer id="home">
-            <HeroBg>
-                <VideoEff autoPlay loop muted src={video} type='video/mp4' />
-            </HeroBg>
-            <HeroContent>
-                <Heroh1>Christina Mueller</Heroh1>
-                <Heroul>
-                <li><a onClick={() => window.open('https://www.linkedin.com/in/christina-pagan-19889b1aa/')}>LinkedIn</a></li>
-                <li><a onClick={() => window.open('https://github.com/cpagan415')}>Github</a></li>
-                </Heroul>
-            </ HeroContent>
-        </HeroContainer>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.15 }} exit={{ opacity: 0 }}>
+            <HeroContainer id="home">
+                <HeroBg>
+                    <VideoEff autoPlay loop muted src={video} type='video/mp4' />
+                </HeroBg>
+                <HeroContent>
+                    <Heroh1>Christina Mueller</Heroh1>
+                    <Heroul>
+                        <li><a onClick={() => window.open('https://www.linkedin.com/in/christinamueller3000')}>LinkedIn</a></li>
+                        <li><a onClick={() => window.open('https://github.com/cpagan415')}>Github</a></li>
+                    </Heroul>
+                </ HeroContent>
+            </HeroContainer>
         </motion.div>
     )
 }
